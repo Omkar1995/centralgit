51f6bb0c7e139e106be0257472fa8c94f78df220
Omkar1995 <86733721+Omkar1995@users.noreply.github.com>
2021-07-01 11:05:20 +0530

    Create helloomkar.c

---
+++ helloomkar.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+  int a;
+  int b;
+  int c;
+  scanf("enter first number: %d", &a);
+  scanf("enter second number: %d", &b);
+  c = a + b;
+  printf("result is: %d+%d=%d\n", a, b, c);
+}
